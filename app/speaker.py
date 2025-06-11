6b8f882b97aabc5c6bbe033c5c57d57bccc358c7
bryan galindo <bryanrgd@gmail.com>
2025-06-11 12:50:28 -0400

    refactor

--- app/speaker.py
+++ app/speaker.py
@@ -3,9 +3,6 @@ from pydub import AudioSegment
 import os
 
 # 🔧 Ruta a tu instalación de ffmpeg (ajústala según tu carpeta real)
-os.environ["PATH"] += os.pathsep + r"C:\ffmpeg-7.1.1-essentials_build\bin"
-AudioSegment.converter = r"C:\ffmpeg-7.1.1-essentials_build\bin\ffmpeg.exe"
-AudioSegment.ffprobe = r"C:\ffmpeg-7.1.1-essentials_build\bin\ffprobe.exe"
 
 # 🔐 Credenciales AWS (usa variables de entorno en producción real)
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
@@ -38,14 +35,17 @@ def generar_audio(texto, filename_wav='respuesta.wav', voz='Pedro'):
 
         # Convertir a WAV
         audio = AudioSegment.from_file(filename_mp3, format='mp3')
-        audio.export("audios/"+filename_wav, format='wav')
+        os.makedirs("audios", exist_ok=True)
+        audio.export(os.path.join("audios", filename_wav), format='wav')
+
 
         # Eliminar temporal
         os.remove(filename_mp3)
 
         print(f"✅ Audio generado en WAV: {filename_wav}")
         
-        return os.path.join("../audios", filename_wav) 
+        return os.path.join("audios", filename_wav)
+
 
     except Exception as e:
         print(f"❌ Error generando audio: {e}")
